411ae3929e7190297baba90ace0a5a070c2b9693
ValentinaPol <polianskaya.valentina@gmail.com>
2023-01-08 23:03:50 +0300

    Persistent Bugger other cases

--- JS/Persistent_Bugger.js
+++ JS/Persistent_Bugger.js
@@ -16,3 +16,29 @@ function persistence(num) {
 console.log(persistence(39));
 console.log(persistence(999));
 console.log(persistence(4));
+
+/*function persistence(num) {
+    var times = 0;
+    
+    num = num.toString();
+    
+    while (num.length > 1) {
+      times++;
+      num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+    }
+    
+    return times;
+}*/
+
+/*const persistence = num => {
+    return `${num}`.length > 1 
+      ? 1 + persistence(`${num}`.split('').reduce((a, b) => a * +b)) 
+      : 0;
+}*/
+
+/*function persistence(num) {
+    for (var i = 0; num > 9; i++) {
+      num = num.toString().split('').reduce((t, c) => c * t);
+    }
+    return i;
+} */
