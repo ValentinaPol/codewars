63723da12412f4b614275ec0603f5ab232323b95
ValentinaPol <polianskaya.valentina@gmail.com>
2023-01-17 00:02:09 +0300

    The Hashtag Generator other cases

--- JS/The_Hashtag_Generator.js
+++ JS/The_Hashtag_Generator.js
@@ -18,3 +18,21 @@ function generateHashtag (str) {
 console.log(generateHashtag(" Hello there thanks for trying my Kata"));
 console.log(generateHashtag("  "));
 console.log(generateHashtag("    Hello     World   "));
+
+/*function generateHashtag (str) {
+    if(!str || str.length < 1) return false;
+    
+    var r = '#' + str.split(' ').map(function(el) {
+      return el.charAt(0).toUpperCase() + el.slice(1).toLowerCase();
+    }).join('');
+    return r.length > 140?false:r;
+}*/
+
+/*function generateHashtag (str) {
+
+    var hashtag = str.split(' ').reduce(function(tag, word) {
+      return tag + word.charAt(0).toUpperCase() + word.substring(1);
+    }, '#');
+    
+    return hashtag.length == 1 || hashtag.length > 140 ? false : hashtag;
+}*/
